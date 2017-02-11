635cc88e1463c022db7a4eb5d17a64cba4255a49
Ossey <xiaoyuan1314@me.com>
2017-02-11 14:59:24 +0800

    添加UIScrollView分类，结合XYLoadingView使用，当加载数据时，显示加载提示信息！

---
+++ MVVMDemo/MVVMDemo/Classes/Example3/View/Example3VC.m
@@ -0,0 +1,37 @@
+//
+//  Example3VC.m
+//  MVVMDemo
+//
+//  Created by mofeini on 17/2/11.
+//  Copyright © 2017年 com.test.demo. All rights reserved.
+//
+
+#import "Example3VC.h"
+
+@interface Example3VC ()
+
+@end
+
+@implementation Example3VC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ MVVMDemo/MVVMDemo/Classes/Utils/Loading/UIScrollView+XYLoading.m
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+XYLoading.m
+//  MVVMDemo
+//
+//  Created by mofeini on 17/2/11.
+//  Copyright © 2017年 com.test.demo. All rights reserved.
+//
+
+#import "UIScrollView+XYLoading.h"
+
+@implementation UIScrollView (XYLoading)
+
+@end
