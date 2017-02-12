848a59076ecc1c483b014faa862c80bbc94f00c4
Ossey <xiaoyuan1314@me.com>
2017-02-12 21:14:11 +0800

    添加XYCollectionViewModelProtocol协议，用户制定 绑定CollectionView的代理和数据源方法的规范

--- MVVMDemo/MVVMDemo/Classes/Example2/ViewModel/SecondTableView_ViewModel.h
+++ MVVMDemo/MVVMDemo/Classes/Example2/ViewModel/SecondTableView_ViewModel.h
@@ -6,13 +6,8 @@
 //  Copyright © 2017年 com.test.demo. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "XYTableViewModelProtocol.h"
 
-@interface SecondTableView_ViewModel : NSObject <UITableViewDelegate, UITableViewDataSource>
+@interface SecondTableView_ViewModel : NSObject <XYTableViewModelProtocol>
 
-/// 传入一个tableView，内部设置其代理和数据源为SecondTableView_ViewModel类对象
-- (void)handleWithTableView:(UITableView *)tableView;
-
-/// 获取模型数据源
-- (void)getModelListBlock:(NSArray *(^)())modelList completion:(void(^)())completion;
 @end
---
+++ MVVMDemo/MVVMDemo/Reuse/XYCollectionViewModelProtocol.h
@@ -0,0 +1,13 @@
+//
+//  XYCollectionViewModelProtocol.h
+//  MVVMDemo
+//
+//  Created by mofeini on 17/2/12.
+//  Copyright © 2017年 com.test.demo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol XYCollectionViewModelProtocol <NSObject>
+
+@end
