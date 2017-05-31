5e0be231dd40110e8f10b0fd49e5fafd5803d77e
Ossey <xiaoyuan1314@me.com>
2017-05-31 22:09:52 +0800

    修复及删除多余文件

--- MVVMDemo/MVVMDemo/Classes/Libs/ImageViewer/XYImageBrowerView.h
+++ MVVMDemo/MVVMDemo/Classes/Libs/ImageViewer/XYImageBrowerView.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 
-@class XYImageBrowerView, XYImagePageLabel;
+@class XYImageBrowerView, XYImagePageLabel, XYImageView, XYImageProgressView;
 
 @protocol XYImageBrowerViewDelegate <NSObject>
 
@@ -94,6 +94,8 @@
 
 @end
 
+
+
 @interface XYImageBrowerView : UIView
 
 
@@ -133,5 +135,66 @@
 
 @interface XYImagePageLabel : UILabel
 
+
+@end
+
+
+@protocol XYImageViewDelegate <NSObject>
+
+- (void)imageViewTouch:(XYImageView *)imageView;
+
+- (void)imageView:(XYImageView *)imageView scale:(CGFloat)scale;
+
+- (void)imageViewDidScrollTopOrBottom:(XYImageView *)imageView;
+
+- (void)imageViewDidEndDragging:(XYImageView *)imageView;
+
+@end
+
+
+
+@interface XYImageView : UIScrollView
+
+/// 当前视图所在的索引
+@property (nonatomic, assign) NSInteger index;
+/// 图片的大小
+@property (nonatomic, assign) CGSize pictureSize;
+/// 显示的默认图片
+@property (nonatomic, strong) UIImage *placeholderImage;
+/// 图片的地址 URL
+@property (nonatomic, strong) NSString *urlString;
+/// 当前显示图片的控件
+@property (nonatomic, strong, readonly) UIImageView *imageView;
+/// 代理
+@property (nonatomic, weak) id<XYImageViewDelegate> imageViewDelegate;
+
+
+/**
+ *  动画显示
+ *
+ * @param   rect            从哪个位置开始做动画
+ * @param   animationBlock  附带的动画信息
+ * @param   completionBlock 结束的回调
+ */
+- (void)animationShowWithFromRect:(CGRect)rect duration:(CGFloat)duration  animationBlock:(void(^)())animationBlock completionBlock:(void(^)())completionBlock;
+
+
+/**
+ *  动画消失
+ *
+ * @param   rect            回到哪个位置
+ * @param   animationBlock  附带的动画信息
+ * @param   completionBlock 结束的回调
+ */
+- (void)animationDismissWithToRect:(CGRect)rect duration:(CGFloat)duration  animationBlock:(void(^)())animationBlock completionBlock:(void(^)())completionBlock;
+
+
+@end
+
+@interface XYImageProgressView : UIView
+
+@property (nonatomic, assign) CGFloat progress;
+
+- (void)showError;
 
 @end
